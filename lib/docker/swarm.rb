8f757e4c9761c9d6ecb0003721ce1798fbd6f757
Adam <adam@mediadrive.ca>
2018-05-03 16:25:15 +0000

    Add more methods for Docker swarms

--- lib/docker/swarm.rb
+++ lib/docker/swarm.rb
@@ -7,5 +7,24 @@ class Docker::Swarm
     new(conn, Docker::Util.parse_json(conn.get('/swarm')))
   end
 
+  def self.init(opts = {}, conn = Docker.connection)
+    conn.post('/swarm/init', {}, :body => opts.to_json)
+    inspect(conn)
+  end
+
+  def self.join(opts = {}, conn = Docker.connection)
+    resp = conn.post('/swarm/join', {}, :body => opts.to_json)
+    hash = Docker::Util.parse_json(resp) || {}
+    new(conn, hash)
+  end
+
+  def self.leave(force = false, conn = Docker.connection)
+    query = {
+      "force" => force
+    }
+
+    !!conn.post('/swarm/leave', query)
+  end
+
   private_class_method :new
 end
