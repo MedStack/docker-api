9e76509d5b83d2cb8d127aabce0703f65dbbab03
Adam Daniels <adam@mediadrive.ca>
2021-04-29 10:39:32 -0400

    Docker::Task.get

--- lib/docker/task.rb
+++ lib/docker/task.rb
@@ -6,4 +6,9 @@ class Docker::Task
     hashes = Docker::Util.parse_json(conn.get("/tasks", opts))
     Array(hashes).map { |hash| new(conn, hash) }
   end
+
+  def self.get(id, conn = Docker.connection)
+    resp = conn.get("/tasks/#{id}")
+    new(conn, Docker::Util.parse_json(resp) || {})
+  end
 end
